8cee832d616f4988ad8c90e3d0e0703435e2e095
besufikad <07yilmabese@gmail.com>
2022-12-01 21:58:02 +0300

    python argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 from sys import argv
 
-if len(argv) - 1 == 0:
-   print(F"{len(argv) - 1} arguments.")
-elif len(argv) - 1 == 1:
-    print(F"{len(argv) - 1} arguments:\n1: {argv[1]}")
-else:
-    print(F"{len(argv) } arguments:")
-    for i in range(len(argv) ):
-        print(F"{i+1}: {argv[i+1]}")
+
+def main():
+    if len(argv) - 1 == 0:
+        print("0 arguments.")
+        return
+    elif len(argv) - 1 == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+    for i in range(len(argv) - 1):
+        print("{}: {}".format(i + 1, argv[i + 1]))
+
+
+if __name__ == '__main__':
+    main()
