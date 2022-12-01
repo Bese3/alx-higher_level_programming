7c3d452bfb805669aca817f250ca2fd0571dac28
besufikad yilma <07yilmabese@gmail.com>
2022-12-01 11:13:54 -0800

    python infinite sum

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+def main():
+    sum = 0
+    for i in range(len(argv) -1):
+       sum += int(argv[i+1])
+    print(F"{sum}")   
+
+if __name__ == '__main__':
+    main()
