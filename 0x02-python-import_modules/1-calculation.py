ee936b77e54fde7d8e8bcb852d3a0878a7a9a056
besufikad yilma <07yilmabese@gmail.com>
2022-12-01 10:28:36 -0800

    python import & modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add , sub , mul , div
+
+def main():
+  a = 10
+  b = 5
+  print(F"{a} + {b} = {add}")
+  print(F"{a} - {b} = {sub}")
+  print(F"{a} * {b} = {mul}")
+  print(F"{a} / {b} = {div}")
+
+
+if __name__ == '__main__':
+    main()
