23023fe8e0c4811683f68467435820f71d9674fc
besufikad yilma <07yilmabese@gmail.com>
2022-12-01 10:54:52 -0800

    python argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+if len(argv) - 1 == 0:
+   print(F"{len(argv) - 1} arguments.")
+elif len(argv) - 1 == 1:
+    print(F"{len(argv) - 1} arguments:\n1: {argv[1]}")
+else:
+    print(F"{len(argv) } arguments:")
+    for i in range(len(argv) ):
+        print(F"{i}: {argv[i]}")
