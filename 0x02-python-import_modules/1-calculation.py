b2a8adc1bac57ea36a0751df42536f4038195255
besufikad <07yilmabese@gmail.com>
2022-12-05 17:03:37 +0300

    python import modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, div, mul, sub
 
+if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
 
-def main():
     a = 10
     b = 5
 
@@ -10,7 +11,3 @@ def main():
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-
-
-if __name__ == '__main__':
-    main()
