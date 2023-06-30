9671f782c4cb13874447389c588e1942de8453ed
besufikad <07yilmabese@gmail.com>
2023-06-30 11:52:24 +0300

    size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# The Square class represents a square shape.
+class Square:
+     
+     def __init__(self , size):
+        """
+        The above function is a constructor that initializes an object with a given size.
+        
+        :param size: The size parameter is used to specify the size of an object or data structure. It is
+        typically an integer value that represents the number of elements or the length of the object
+        """
+        self.__size = size
+
+    
