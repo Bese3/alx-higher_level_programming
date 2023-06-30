4073b5a481940b2172a19c4b7bb7669ccb0e99ee
besufikad <07yilmabese@gmail.com>
2023-06-30 12:16:50 +0300

    setter&getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!usr/bin/python3
+
+# The Square class represents a square shape.
+class Square:
+    def __init__(self , size=0):
+        """
+        The above code defines a class with a constructor that initializes a size attribute, a method to
+        calculate the area of a square, and getter and setter methods for the size attribute.
+        
+        :param size: The "size" parameter is used to specify the size of an object. It is an integer
+        value that represents the length or width of the object. The size must be a non-negative integer,
+        defaults to 0 (optional)
+        """
+        if type(size) is  not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        return (self.__size)**2
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self , value):
+        if type(value) is  not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = value
