7d3a50e1a96a07b641c42340acd1fe204e94ecbc
besufikad <07yilmabese@gmail.com>
2023-07-02 12:00:19 +0300

    matrix_divided and say_my_name

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
 def matrix_divided(matrix, div):
+    """
+    The function `matrix_divided` takes a matrix and a divisor as input, and returns a new matrix where
+    each element is divided by the divisor and rounded to two decimal places.
+    
+    :param matrix: The matrix parameter is a list of lists representing a matrix. Each inner list
+    represents a row in the matrix, and the elements of the inner lists represent the values in each
+    row. The matrix can contain integers or floats
+    :param div: The div parameter is the number by which each element in the matrix will be divided
+    :return: a new matrix that is the result of dividing each element of the input matrix by the input
+    divisor.
+    """
     for rows in matrix:
         for i in rows:
             if type(i) is not int and type(i) is not float:
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+    """
+    The function "say_my_name" takes in a first name and an optional last name as parameters and prints
+    out a statement with the provided names.
+    
+    :param first_name: The `first_name` parameter is a required parameter that should be a string
+    representing the first name of a person
+    :param last_name: The `last_name` parameter is an optional parameter that represents the last name
+    of a person. If no value is provided for `last_name`, it will default to an empty string
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
