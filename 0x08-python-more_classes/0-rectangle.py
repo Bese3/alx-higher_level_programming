2830c95299f5706a2f2021e8868005b1b1843e24
besufikad <07yilmabese@gmail.com>
2023-07-03 14:16:03 +0300

    0-rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+  The code `class Rectangle: pass` is defining a class named `Rectangle` with no attributes or
+  methods. The `pass` statement is a placeholder that allows the class definition to be syntactically
+  correct, but it does not do anything.
+"""
+class Rectangle:
+     pass
