10215c7abc17f3fc581abf81777b381f98cb3b36
besufikad <07yilmabese@gmail.com>
2023-07-03 20:37:33 +0300

    9-rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -125,12 +125,11 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
+
     @classmethod
     def square(cls, size=0):
         """
         The function "square" returns a rectangle object with
         equal width and height, based on the given size parameter.
         """
-        return Rectangle(size , size)
-
-
+        return Rectangle(size, size)
