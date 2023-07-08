179a172fd39d29e23acec645939ab73c98e54495
besufikad <07yilmabese@gmail.com>
2023-07-08 12:29:45 +0300

    hello_world

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_f_3 = word[:3]
+word_l_2 = word[-2:]
+middle = word[1:-1]
+print("First 3 letters: {}".format(word_f_3))
+print("Last 2 letters: {}".format(word_l_2))
+print("Middle word: {}".format(middle))
