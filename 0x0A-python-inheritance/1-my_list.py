cfe14209ffbcb577689a15f852bbf94a7bf97307
besufikad <07yilmabese@gmail.com>
2023-07-10 11:40:46 +0300

    my_list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+The class `MyList` is a subclass of `list`
+that adds a method `print_sorted` to print a sorted
+list
+"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """
+        The function takes a list, creates a new sorted list,
+        and then prints the sorted list.
+        """
+        new_list = self[:]
+        new_list.sort()
+        print(new_list)
