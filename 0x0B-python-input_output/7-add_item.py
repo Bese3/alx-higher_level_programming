ad2a1e5b9386c337fad6b9a1379cb562b5b43ee1
besufikad <07yilmabese@gmail.com>
2023-07-11 21:43:49 +0300

    7-add_item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""
-@add_item: a method for updating a file without  overwriting
-"""
-from sys import argv
+"""Add all arguments to a Python list and save them to a file."""
+import sys
 
-
-if __name__ == '__main__':
-    """
-    this is part of code is going to run if this module is called directly
-    """
-    load_from_json_file =\
-        __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    filename = "add_item.json"
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
     try:
-        new_list = load_from_json_file(filename)
+        new = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        new_list = []
-    new_list.extend(argv[1:])
-    save_to_json_file(new_list, filename)
+        new = []
+    new.extend(sys.argv[1:])
+    save_to_json_file(new, "add_item.json")
