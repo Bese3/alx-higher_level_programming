3b730276d79aeb9bf2ca5a0e81a08e8a9103d0eb
besufikad <07yilmabese@gmail.com>
2023-07-11 13:39:31 +0300

    read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+reading text from a file
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints it to the standard output
+
+    Args:
+        filename (str): name of the file to open and read
+    """
+    if filename == '' or filename is None:
+        return
+    with open(filename, mode="r") as f:
+        print(f.read())
