eaab36be4c5b67fcf004540dac90ebc495958bb3
besufikad <07yilmabese@gmail.com>
2023-07-17 23:49:35 +0300

    test_modification

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,34 +10,21 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    The `Square` class is a subclass of `Rectangle`
-    that represents a square object with a given size,
-    position, and optional id.
-    """
+    """By inheriting the characteristics of Rectangle, defines a Square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        The above function is a constructor that initializes
-        an object with a given size, position, and optional id.
-        """
+        """Initializes all attributes passed with their respective arguments"""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """
-        The size function returns the width of an object.
-        :return: The width of the object.
-        """
+        """Get width of Rectangle"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """
-        The function sets the width and height of
-        an object to a given value, but raises errors if the
-        value is not an integer or is less than or equal to zero.
-        """
+        """Set width to value, raises exception if value is
+        not an integer"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -46,34 +33,37 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
+        """Updates some or all of the attributes based on how many arguments
+        are passed
+        If args' list index gets out of range i.e. it gets to the end of the
+        list, the updating of the attributes gets done
+
+        If args is empty, the values in kwargs are used to update the
+        attributes' values"""
         if args:
             try:
                 self.id = args[0]
-                self.size = args[1]
+                self.width = args[1]
+                self.height = args[1]
                 self.x = args[2]
                 self.y = args[3]
             except IndexError:
                 pass
         else:
-            for i, j in kwargs.items():
-                setattr(self, i, j)
+            for key in kwargs.keys():
+                try:
+                    if key == "size":
+                        self.__setattr__("width", kwargs[key])
+                        self.__setattr__("height", kwargs[key])
+                        continue
+                    self.__setattr__(key, kwargs[key])
+                except AttributeError:
+                    pass
 
     def to_dictionary(self):
-        """
-        The function `to_dictionary` returns
-        a dictionary with the attributes of an object.
-        """
-        first = {'id': self.id, 'x': self.x, 'size': self.size}
-        second = {'y': self.y}
-        first.update(second)
-        return first
+        """Returns a key-value pair of all attributes and their values"""
+        return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
 
     def __str__(self):
-        """
-        The function returns a string representation
-        of a Rectangle object, including its id,coordinates,
-        width, and height.
-        """
-        string = F"[Square] ({self.id}) {self.x}/{self.y}" + \
-            F" - {self.width}"
-        return string
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                 self.y, self.width)
