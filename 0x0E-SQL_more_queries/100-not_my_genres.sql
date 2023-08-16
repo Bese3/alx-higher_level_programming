a32fa22e7c0e48191cbf3657914879d129bc59ad
besufikad <07yilmabese@gmail.com>
2023-08-16 20:29:56 +0300

    not genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,5 +1,4 @@
--- a script that uses the hbtn_0d_tvshows database to list
-SELECT tv_genres.name FROM  (SELECT * FROM tv_shows JOIN tv_show_genres tsg ON
-    tv_shows.id = tsg.show_id WHERE title = 'Dexter') AS tmp_table
-RIGHT JOIN tv_genres ON tmp_table.genre_id = tv_genres.id WHERE tv_genres.id IS NULL
-ORDER BY tv_genres.name;
+-- a script that uses the hbtn_0d_tvshows 
+SELECT DISTINCT name FROM (SELECT DISTINCT id, title, genre_id FROM tv_shows JOIN tv_show_genres tsg ON
+    tv_shows.id = tsg.show_id WHERE title <> 'Dexter') AS dexter_genre INNER
+    JOIN tv_genres ON dexter_genre.genre_id = tv_genres.id ORDER BY name;
