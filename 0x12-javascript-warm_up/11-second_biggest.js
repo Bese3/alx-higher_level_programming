82dad3b8262576207dd023b0cb0d54b442476dbb
besufikad <07yilmabese@gmail.com>
2023-09-11 19:50:25 +0300

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,17 +3,10 @@ const argv = require('process').argv;
 if (!argv[2] || argv.length === 3) {
   console.log('0');
 } else {
-  let biggest = 0;
-  for (let i = 0; i < argv.length; i++) {
-    if (argv[i] > biggest) {
-      biggest = i;
-    }
+  const num = [];
+  for (let i = 2; i < argv.length; i++) {
+    num.push(parseInt(argv[i]));
   }
-  argv[biggest] = 0;
-  for (let i = 0; i < argv.length; i++) {
-    if (argv[i] > biggest) {
-      biggest = argv[i];
-    }
-  }
-  console.log(biggest);
+  num.sort();
+  console.log(num[num.length - 2]);
 }
