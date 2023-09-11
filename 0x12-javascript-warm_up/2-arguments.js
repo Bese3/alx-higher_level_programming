70ab0c696ee212bb1943f5558b7160e91e3d3556
besufikad <07yilmabese@gmail.com>
2023-09-11 17:33:46 +0300

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@ const argv = require('process').argv;
 
 if (argv.length === 2) {
   console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
 } else {
   console.log('Argument found');
 }
