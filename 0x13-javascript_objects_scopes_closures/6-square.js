4442867473cd239c333f085ce49012dbdcab0308
besufikad <07yilmabese@gmail.com>
2023-09-12 22:26:33 +0300

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,29 +1,17 @@
 #!/usr/bin/node
 // class Square that defines a square and inherits from Rectangle of 4-rectangle.js
 
-const Rectangle = require('./4-rectangle');
+const SquareBase = require('./5-square');
 
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
-  /**
-   * charPrint - prints a rectangle of characters with a specified width and height, using
-   * the character `c` as the fill.
-   * @c: - The parameter `c` is a character that will be used to print a pattern. If no character
-   * is provided, the default character 'X' will be used.
-   */
+class Square extends SquareBase {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      let myStr = '';
-      for (let j = 0; j < this.width; j++) {
-        myStr += c;
-      }
-      console.log(myStr);
+      console.log(c.repeat(this.width));
     }
   }
-};
+}
+
+module.exports = Square;
