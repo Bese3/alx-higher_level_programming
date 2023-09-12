1bcbe3e4e9d8c4cf72527efa6c8ae972a7c82dde
besufikad <07yilmabese@gmail.com>
2023-09-12 21:13:24 +0300

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myStr = '';
+      for (let j = 0; j < this.width; j++) {
+        myStr += 'X';
+      }
+      console.log(myStr);
+    }
+  }
+};
