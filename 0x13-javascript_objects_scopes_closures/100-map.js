4e0d3275b7f21cb69e7870fe49462f9c1450b10e
besufikad <07yilmabese@gmail.com>
2023-09-13 22:06:41 +0300

    task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const myArray = require('./100-data').list;
-const doubleArray = [];
-for (let i = 0; i < myArray.length; i++) {
-  doubleArray.push(myArray[i] * i);
-}
+const doubleArray = myArray.map((element, index) => element * index);
+
 console.log(myArray);
 console.log(doubleArray);
