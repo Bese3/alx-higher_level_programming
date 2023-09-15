f4e8ca4d5cf8434b59ae51d79ac813bdee9d60b6
besufikad <07yilmabese@gmail.com>
2023-09-15 16:40:14 +0300

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    A script that lists all State objects from the database hbtn_0e_6_usa
+    Username, password and dbname wil be passed as arguments to the script.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+def main():
+    """
+    The main function connects to a MySQL database,
+    creates tables if they don't exist, retrieves and
+    prints all states from the database, and then closes the session.
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    Session = session()
+    states = Session.query(State).order_by('id').first()
+    # for state in states:
+    print(f"{states.id}: {states.name}")
+    Session.close()
+
+
+if __name__ == '__main__':
+    main()
