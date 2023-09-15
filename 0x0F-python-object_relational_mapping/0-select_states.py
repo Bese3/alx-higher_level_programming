20490d307baa3cf9821722fdc40f00546b47b69e
besufikad <07yilmabese@gmail.com>
2023-09-15 12:36:42 +0300

    task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,16 +11,13 @@ def main():
     """
     main program starts here
     """
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306,
-                             user=argv[1], passwd=argv[2], db=argv[3])
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM state ORDER BY id")
-        for i in cursor.fetchall():
-            print(i)
-    except IndexError:
-        pass
-
+    db = MySQLdb.connect(host="localhost", port=3306,
+                        user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM state ORDER BY id")
+    for i in cursor.fetchall():
+          print(i)
+   
 
 if __name__ == '__main__':
     main()
