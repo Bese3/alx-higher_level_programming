2cee03af00abe673f56ecb4a06c144d3cf95e207
besufikad <07yilmabese@gmail.com>
2023-09-15 16:51:09 +0300

    task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    A script that lists all State objects from the database hbtn_0e_6_usa
+    Username, password and dbname wil be passed as arguments to the script.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+def main():
+    """
+    The main function connects to a MySQL database,
+    creates tables if they don't exist, retrieves and
+    prints all states from the database, and then closes the session.
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    Session = session()
+    states = Session.query(State).\
+        filter(State.name.ilike("%a%")).order_by('id')
+    if states:
+        for state in states:
+            print(f"{state.id}: {state.name}")
+    else:
+        print("Nothing")
+    Session.close()
+
+
+if __name__ == '__main__':
+    main()
