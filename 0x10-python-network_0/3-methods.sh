f924e4cafbbcb862c1a262b008b2ab1962d09903
besufikad <07yilmabese@gmail.com>
2023-09-29 03:42:21 -0700

    task 4

---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,8 @@
+#1/bin/bash
+# a Bash script that takes in a URL
+# and displays all HTTP methods the server will accept.
+if [ $# -lt 1 ]; then
+	exit
+fi
+curl -sIX OPTIONS $1 | grep Allow | sed 's/Allow: //'
+
