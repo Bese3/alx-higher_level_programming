649360b9c5e5d81185db03b1d884a7c5ba96beb0
besufikad <07yilmabese@gmail.com>
2023-09-29 22:37:16 -0700

    edited all sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# a Bash script that sends a DELETE request to the URL passed
-# as the first argument and displays the body of the response
+# a Bash script that sends a DELETE request to the URL passes
 curl -X DELETE "$1"
-
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,3 @@
 #1/bin/bash
 # a Bash script that takes in a URL
-# and displays all HTTP methods the server will accept.
 curl -sIX OPTIONS "$1" | grep Allow | sed 's/Allow: //'
-
