b3e2ace5dc40fe20cfbb25de496f8a7894df3362
besufikad <07yilmabese@gmail.com>
2023-10-01 11:55:00 +0300

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
+"""
+block is a common idiom in Python that allows
+a script to be executed as a standalone
+program or imported as a module.
+"""
 import urllib.request
 
-"""
- The `if __name__ == '__main__':` block is a common idiom
- in Python that allows a script to be executed as a standalone
- program or imported as a module.
-"""
-if __name__ == '__main__':
-    """
-    requesting for alx status
-    """
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status")\
+
+def main():
+    '''Program starts here.
+    Details about the http response
+    are printed.
+    '''
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url)\
          as response:
+        response_read = response.read()
         print("Body response:")
-        print(f"\t- type: {type(response.read())}")
-        print(f"\t- content: {response.read()}")
-        print(f"\t- utf8 content: {str(response.read())[2:-1]}")
+        print("\t- type:", type(response_read))
+        print("\t- content:", response_read)
+        print("\t- utf8 content:", str(response_read)[2:-1])
+
+
+if __name__ == '__main__':
+    main()
