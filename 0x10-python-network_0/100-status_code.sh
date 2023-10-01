2d75196824a99928372471267094f67be86f91c7
besufikad <07yilmabese@gmail.com>
2023-10-01 10:54:33 -0700

    advanced_task_0

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a request to a URL passed as an argument
-curl -sI "$1" | head -n 1 | grep -v [a-z] | tr -d '[A-Z]' | sed 's-/[0-9].[0-9] --' | tr -d '\n'
+curl -s -o -I -w "%{http_code}" "$1"
