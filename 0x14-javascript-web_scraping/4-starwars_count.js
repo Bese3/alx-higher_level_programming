24283fdc3edbc0cbe0564abf9f688d39565e71a5
besufikad <07yilmabese@gmail.com>
2023-10-24 19:32:41 +0300

    t 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
+// const { json } = require('stream/consumers');
 const argument = process.argv;
 const url = argument[2];
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    let i = 0;
     let count = 0;
-    while (i < JSON.parse(body).count) {
-      const characters = JSON.parse(body).results[i].characters;
-      for (let character = 0; character < characters.length; character++) {
-        if (characters[character] === 'https://swapi-api.alx-tools.com/api/people/18/') {
-          count += 1;
-        }
+    const results = JSON.parse(body).results;
+    results.forEach((i) => {
+      if (i.characters.indexOf('https://swapi-api.alx-tools.com/api/people/18/') !== -1) {
+        count += 1;
       }
-      i += 1;
-    }
+    });
     console.log(count);
   }
 });
