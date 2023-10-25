5daf9854bcc39a550ea761b05abd0fd8c9e3343a
besufikad <07yilmabese@gmail.com>
2023-10-25 11:47:40 +0300

    t 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,33 @@
 #!/usr/bin/node
-// Computes the number of tasks completed by user id.
 
 const request = require('request');
-const url = process.argv[2];
+const argument = process.argv;
 
-request(url, (error, response, body) => {
+request(argument[2], (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const json = JSON.parse(body);
-    const userIdCompletion = {};
-    json.forEach(todo => {
-      if (todo.completed) {
-        if (userIdCompletion[todo.userId] !== undefined) {
-          userIdCompletion[todo.userId] += 1;
-        } else userIdCompletion[todo.userId] = 1;
-      }
-    });
+    // console.log(JSON.parse(body))
+    let taskCompleted = 0;
+    const myJson = JSON.parse(body);
+    const myDict = {};
+    for (let i = 0; i < myJson.length; i++) {
+      try {
+        if (i === myJson.length - 1) {
+          myDict[myJson[i].userId] = taskCompleted;
+        } else if (myJson[i - 1].userId !== myJson[i].userId) {
+          myDict[myJson[i - 1].userId] = taskCompleted;
+          taskCompleted = 0;
+        }
+      } catch (error) {
 
-    console.log(userIdCompletion);
+      } finally {
+        if (myJson[i].completed === true) {
+          taskCompleted += 1;
+        }
+      }
+    }
+
+    console.log(myDict);
   }
 });
