0c8437588d8d0e2cdf5f89f6e85c99f9d149cd88
besufikad <07yilmabese@gmail.com>
2023-11-01 19:21:15 +0300

    4-js

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+$('#toggle_header').on('click', () => {
+    if ($('header').hasClass('red')){
+        $('header').removeClass('red');
+        $('header').addClass('green');
+    } else {
+        $('header').removeClass('green');
+        $('header').addClass('red');
+    }
+});
